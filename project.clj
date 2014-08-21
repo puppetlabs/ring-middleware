17e7ef96c1a2b3a4d46c635f9a8a650c21addc35
Chris Price <chris@puppetlabs.com>
2014-08-21 14:20:44 -0700

    Initial files

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject puppetlabs/ring-middleware "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  ;; Abort when version ranges or version conflicts are detected in
+  ;; dependencies. Also supports :warn to simply emit warnings.
+  ;; requires lein 2.2.0+.
+  :pedantic? :abort
+
+  :plugins [[lein-release "1.0.5"]]
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]
+                        ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
+  )
---
+++ test/default/core_test.clj
@@ -0,0 +1,7 @@
+(ns default.core-test
+  (:require [clojure.test :refer :all]
+            [default.core :refer :all]))
+
+(deftest a-test
+  (testing "DELETE ME, I'm worthless."
+    (is (= 0 0))))
