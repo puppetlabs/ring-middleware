116cbf957755c76aca286b17c7472be9a3deb620
Lindsey Smith <lindsey@puppetlabs.com>
2014-08-25 18:25:46 -0700

    (PE-5653) Correctly proxy non-GET methods
    Some code we brought across from the original ring-proxy seemed to be
    specifically for clj-http, and it was causing POST/PUT requests to
    fail. This commit removes that code and standardises on `slurp`'ing the
    incoming request body before sending it to the proxied server.

--- src/puppetlabs/ring_middleware/common.clj
+++ src/puppetlabs/ring_middleware/common.clj
@@ -9,17 +9,3 @@
   (let [prepare #(-> (update-in % [1 :expires] str)
                      (update-in [1] dissoc :domain :secure))]
     (assoc resp :cookies (into {} (map prepare (:cookies resp))))))
-
-(defn slurp-binary
-  "Reads len bytes from InputStream is and returns a byte array."
-  [^java.io.InputStream is len]
-  (with-open [rdr is]
-    (let [buf (byte-array len)]
-      (.read rdr buf)
-      buf)))
-
-(defn http-get
-  ([url]
-   (http-get url {:as :text}))
-  ([url options]
-   (http-client/get url options)))
