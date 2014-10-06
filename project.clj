f902d7d73e20f2f4423aa04c7e7afed367512692
Jenkins CI <ci@puppetlabs.com>
2014-10-06 16:46:43 -0700

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/ring-middleware "0.1.3-SNAPSHOT"
+(defproject puppetlabs/ring-middleware "0.1.3"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ring "1.3.0"]
