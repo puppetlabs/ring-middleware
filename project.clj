024bfd9ff892faee2e428f74fa67e80936ef8deb
Jenkins CI <ci@puppetlabs.com>
2015-03-16 14:38:39 -0700

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/ring-middleware "0.2.1-SNAPSHOT"
+(defproject puppetlabs/ring-middleware "0.2.1"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ring "1.3.0" :exclusions [org.clojure/java.classpath]]
