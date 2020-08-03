66eac195f26f6a7a90166ba75fb0ff90c66bd43a
Jenkins CI <ci@puppet.com>
2020-08-03 23:53:19 +0000

    Version 1.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/ring-middleware "1.2.0"
+(defproject puppetlabs/ring-middleware "1.2.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure]
                  [org.clojure/tools.logging]
                  [cheshire]
